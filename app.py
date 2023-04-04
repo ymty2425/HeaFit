fe880ac22f9c6ee6e574eb90561dea8c956eccae
zhengyuan <zhengyuan@yuanjihongdeMacBook-Pro.local>
2023-04-03 21:32:41 -0400

    Test in API keys, change plan container to embeded chatbot with icon

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import openai
 from flask import Flask, redirect, render_template, request, url_for, session, jsonify
+import logging
 
-# 124500 words allowed for training
+# Add this line after the imports at the top of the file
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -14,93 +16,75 @@ def home():
 @app.route('/personal-info', methods=['GET', 'POST'])
 def personal_info():
     if request.method == 'POST':
-        # Handle form submission and generate plan
-        pass
+        pass     
     return render_template('personal-info.html')
 
 @app.route('/api/generate-plan', methods=['POST'])
 def generate_plan():
-    # Extract the data from the request
-    data = request.json
+    height = request.json.get('height')
+    weight = request.json.get('weight')
+    gender = request.json.get('gender')
+    fitness_goal = request.json.get('fitness_goal')
+    fitness_level = request.json.get('fitness_level')
+    dietary_requirements = request.json.get('dietary_requirements')
 
-    # Generate the workout and nutrition plan using the OpenAI API (or any other API)
-    # The code will depend on the specific API or service you are using
-    # For example, you may call functions that use OpenAI API here
+    # print("Received data:", request.json)
 
-    # Mock plan for demonstration purposes
-    plan = {
-        'workout': 'Sample workout plan',
-        'nutrition': 'Sample nutrition plan',
-    }
+    try:
+        prompt = f"Generate a workout and nutrition plan for a {gender.lower()} with the following details:\n" \
+                 f"Height: {height} cm\n" \
+                 f"Weight: {weight} kg\n" \
+                 f"Fitness goal: {fitness_goal}\n" \
+                 f"Fitness level: {fitness_level}\n" \
+                 f"Dietary requirements: {dietary_requirements}\n\n" \
+                 f"Workout plan:\n"
 
-    return jsonify(plan)
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        plan = response.choices[0].text.strip()
+        print(plan)
+        return jsonify({'plan': plan})
+
+    except Exception as e:
+        logging.exception("Error occurred while generating the plan")
+        return jsonify({'error': str(e)}), 500
+
 
 @app.route('/result')
 def result():
     return render_template('result.html')
 
 @app.route('/api/chat', methods=['POST'])
-def chat_api():
+def chat():
     message = request.json.get('message')
-    chatbot_response = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=f"User: {message}\nAI:",
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    response = chatbot_response.choices[0].text.strip()
-    return jsonify(response=response)
+    if not message:
+        return jsonify({'error': 'No message provided'}), 400
+
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"{message}\n\nAI:",
+            temperature=0.5,
+            max_tokens=100,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        response_message = response.choices[0].text.strip()
+        return jsonify({'response': response_message})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-'''
-@app.route('/generate_plan', methods=['POST'])
-def generate_plan():
-    fitness_goal = request.form.get('fitness_goal')
-    fitness_level = request.form.get('fitness_level')
-    dietary_requirements = request.form.get('dietary_requirements')
-
-    response = openai.Comletion.create(
-        model = "text-davinci-003",
-        workout_plan = generate_prompt(fitness_goal, fitness_level, dietary_requirements),
-        temperature = 0.6,
-    )
-    
-    # can save the plan to the db
-    # session["hea_fit_plan"] = workout_plan
-    return redirect(url_for('plan', result = response.choices[0].text))
-
-@app.route('/plan')
-def plan():
-    # can try to retreive from the db
-    workout_plan = session.get("workout_plan", "")
-    nutrition_plan = session.get("nutrition_plan", "")
-
-    return render_template('plan.html', workout_plan=workout_plan, nutrition_plan=nutrition_plan)
-
-def generate_prompt(goal, level, require):
-    return """My fitness is {}, fitness level is {}, and dietary requirements are {}.
-    Can you please help me generate a work out plan: """.format(
-        goal.capitalize(), level.capitalize(), require.capitalize()
-    )
-'''
-
-'''
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        premise = request.form["premise"]
-        argument = request.form["argument"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(premise, argument),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-'''
--- static/js/main.js
+++ static/js/main.js
@@ -1,98 +1,123 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const personalInfoForm = document.getElementById('personal-info-form');
+    const personalInfoForm1 = document.getElementById('personal-info-form');
     const planElement = document.getElementById('plan');
     const chatbotContainer = document.getElementById('chatbot-container');
-  
-    // Personal info form handling
+    const chatbotContent = document.getElementById('chatbot-content');
+    const messages = document.getElementById('messages');
+    const messageInput = document.getElementById('message-input');
+    const personalInfoForm = document.querySelector('#personal-info-form');
+    
     if (personalInfoForm) {
-      personalInfoForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-  
+      personalInfoForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
         const formData = new FormData(personalInfoForm);
-        const data = Object.fromEntries(formData.entries());
+        const data = Object.fromEntries(formData);
+
+        console.log("Form data:", data);
+
+        try {
+          const response = await fetch('/api/generate-plan', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+          });
+          if (response.ok) {
+            const plan = await response.json();
+    
+            // Save the generated workout plan in the local storage to access it on the result page
+            localStorage.setItem('workoutPlan', plan.plan);
+    
+            // Redirect to the result page
+            window.location.href = "/result";
+          } else {
+            alert('Error generating the workout plan. Please try again.');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          alert('Error generating the workout plan. Please try again.');
+        }
+      });
+    }
+
+    // Display the workout and nutrition plan on the result page
+    if (planElement) {
+      const plan = localStorage.getItem('workoutPlan');
+      localStorage.removeItem('workoutPlan');
   
-        const response = await fetch('/api/generate-plan', {
+      if (plan) {
+        // Replace newline characters with <br> tags
+        const formattedWorkoutPlan = plan.replace(/\n/g, '<br>');
+
+        // Display the plan
+        planElement.innerHTML = `
+          <h2>Workout Plan</h2>
+          <p>${formattedWorkoutPlan}</p>
+        `;
+      } else {
+        // Redirect the user back to the personal-info page if no plan is available
+        planElement.innerHTML = "<p>Error loading the generated workout plan. Please go back and try again.</p>";
+        //window.location.href = '/result';
+      }
+    }
+
+    // Toggle chatbot visibility when the icon is clicked
+    const chatbotIcon = document.getElementById('chatbot-icon');
+    if (chatbotIcon) {
+      chatbotIcon.addEventListener('click', () => {
+        chatbotContainer.classList.remove('collapsed');
+        chatbotIcon.style.display = 'none';
+      });
+    }
+
+    // Close the chatbot when the close button is clicked and show the icon again
+    const chatbotClose = document.getElementById('chatbot-close');
+    if (chatbotClose) {
+      chatbotClose.addEventListener('click', () => {
+        chatbotContainer.classList.add('collapsed');
+        chatbotIcon.style.display = 'block';
+      });
+    }
+  
+    // Initialize the chatbot on the result page
+    if (chatbotContent) {
+      // Function to send a message to the API and receive a response
+      async function sendMessageToAPI(message) {
+        const response = await fetch('/api/chat', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(data),
+          body: JSON.stringify({ message }),
         });
-  
-        if (response.ok) {
-          const plan = await response.json();
-          localStorage.setItem('plan', JSON.stringify(plan));
-          window.location.href = '/result';
-        } else {
-          // Handle error, e.g., display an error message
+        const data = await response.json();
+        return data.response;
+      }
+
+      // Function to add a message to the chat UI
+      function addMessageToChat(sender, message) {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add(sender);
+        messageElement.textContent = message;
+        messages.appendChild(messageElement);
+      }
+
+      // Event listener for the message input field
+      messageInput.addEventListener('keydown', async (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+
+          const message = messageInput.value.trim();
+          if (message) {
+            addMessageToChat('user', message);
+            messageInput.value = '';
+
+            const response = await sendMessageToAPI(message);
+            addMessageToChat('bot', response);
+          }
         }
       });
     }
-  
-    // Display the workout and nutrition plan on the result page
-    if (planElement) {
-      const plan = JSON.parse(localStorage.getItem('plan'));
-      localStorage.removeItem('plan');
-  
-      if (plan) {
-        // Display the plan
-        planElement.innerHTML = `
-          <h2>Workout Plan</h2>
-          <p>${plan.workout}</p>
-          <h2>Nutrition Plan</h2>
-          <p>${plan.nutrition}</p>
-        `;
-      } else {
-        // Redirect the user back to the personal-info page if no plan is available
-        window.location.href = '/personal-info';
-      }
-    }
-  
-    // Initialize the chatbot on the result page
-    if (chatbotContainer) {
-        async function sendMessageToAPI(message) {
-            const response = await fetch('/api/chat', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ message }),
-            });
-            const data = await response.json();
-            return data.response;
-          }
-          
-          // Add this function to handle sending and receiving messages in the chatbot UI
-          function addMessageToChat(sender, message) {
-            const messageElement = document.createElement('div');
-            messageElement.classList.add(sender);
-            messageElement.textContent = message;
-            chatbotContainer.appendChild(messageElement);
-          }
-          
-          // Add an event listener for the chatbot input form
-          const chatbotForm = document.createElement('form');
-          chatbotForm.innerHTML = `
-            <input type="text" id="chatbot-input" placeholder="Type your message...">
-            <button type="submit">Send</button>
-          `;
-          chatbotForm.addEventListener('submit', async (e) => {
-            e.preventDefault();
-          
-            const input = chatbotForm.querySelector('#chatbot-input');
-            const message = input.value.trim();
-          
-            if (message) {
-              addMessageToChat('user', message);
-              input.value = '';
-          
-              const response = await sendMessageToAPI(message);
-              addMessageToChat('bot', response);
-            }
-          });
-          
-          chatbotContainer.appendChild(chatbotForm);
-      }
       
   });
   
